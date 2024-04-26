dabd092ba6945637317e5468288d0e46c04b15e6
maihannn <maicapalillo15@gmail.com>
2024-04-26 16:18:18 +0800

    added odd and even file to store the numbers

--- even_odd.py
+++ even_odd.py
@@ -12,17 +12,35 @@ numbers_list = []
 even_num_list = []
 odd_num_list = []
 
+# Create a file named "numbers.txt" that contains all the numbers.
+
+with open ("numbers.txt", "r") as number_file:
+    for value in number_file:
+        value = int(value.strip())
+        numbers_list.append(value)
+        
 # Ask for users input:
 
 data_input = int (input("Enter a number: "))
 
 for num in range (data_input, data_input + 20):
-    print (num, end = " ")
+    print ("Number List: ", num, end = " ")
+
 # Checking if the number is an odd or an even number:
     if int (num) % 2 == 0:
         even_num_list.append(num)
     else:
         odd_num_list.append (num)
+
+# Create a file named "even.txt" for all the even numbers.
+with open ("even.txt", "w") as even_file:
+    for data in even_num_list:
+        even_file.write(str(data) + "\n")
+
+# Create a file named "odd.txt" for all the odd numbers.
+with open ("odd.txt", "w") as odd_file:
+    for data in odd_num_list:
+        odd_file.write(str(data) + "\n")
 
 print ("\n\nThe Even Numbers detected in the list are: ", even_num_list)
 print ("\nThe Odd Numbers detected in the list are: ", odd_num_list)
