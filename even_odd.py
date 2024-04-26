93bb9d72e23fdd44ea88992e3752fa19d8935037
maihannn <maicapalillo15@gmail.com>
2024-04-26 19:49:47 +0800

    its working

--- even_odd.py
+++ even_odd.py
@@ -6,42 +6,53 @@
 # The program will create two other text file; the first text file will be named even.txt that will contain all even numbers extracted from the numbers.txt
 # The second text file will be named odd.txt that will contain all odd numbers extracted from the numbers.txt
 
-# Variables:
 
-numbers_list = []
-even_num_list = []
-odd_num_list = []
+# Adding a while loop function to allow the users to use the program again.
 
-# Create a file named "numbers.txt" that contains all the numbers.
+while True:
 
-with open ("numbers.txt", "r") as number_file:
-    for value in number_file:
-        value = int(value.strip())
-        numbers_list.append(value)
+    # Variables:
+    even_num_list = []
+    odd_num_list = []
+
+    # Ask for users input:
+
+    try:
+        data_input = int(input("Enter a number: "))
+
+        # Create a file named "numbers.txt" that contains all the numbers.
+        with open("numbers.txt", "w") as number_file:
+            for num in range(data_input, data_input + 20):
+                number_file.write(str(num) + "\n")
+                print(num, end=" ")
+
+                # Checking if the number is an odd or an even number:
+                if num % 2 == 0:
+                    even_num_list.append(num)
+                else:
+                    odd_num_list.append (num)
+
+    # Create a file named "even.txt" for all the even numbers.
+        with open ("even.txt", "w") as even_file:
+            for data in even_num_list:
+                even_file.write(str(data) + "\n")
+
+    # Create a file named "odd.txt" for all the odd numbers.
+        with open ("odd.txt", "w") as odd_file:
+            for data in odd_num_list:
+                odd_file.write(str(data) + "\n")
         
-# Ask for users input:
+        print ("\n\nThe Even Numbers detected in the list are: ", even_num_list)
+        print ("\nThe Odd Numbers detected in the list are: ", odd_num_list)
 
-data_input = int (input("Enter a number: "))
+    # Catch Errors (strings that are not a number)
+    except ValueError:
+        print ("Error! Enter a valid number.")
 
-for num in range (data_input, data_input + 20):
-    print ("Number List: ", num, end = " ")
-
-# Checking if the number is an odd or an even number:
-    if int (num) % 2 == 0:
-        even_num_list.append(num)
+    user_answer = input ("\nDo you want to input another number? y/n ")
+    if user_answer.lower() == "y":
+        continue
     else:
-        odd_num_list.append (num)
+        break
 
-# Create a file named "even.txt" for all the even numbers.
-with open ("even.txt", "w") as even_file:
-    for data in even_num_list:
-        even_file.write(str(data) + "\n")
-
-# Create a file named "odd.txt" for all the odd numbers.
-with open ("odd.txt", "w") as odd_file:
-    for data in odd_num_list:
-        odd_file.write(str(data) + "\n")
-
-print ("\n\nThe Even Numbers detected in the list are: ", even_num_list)
-print ("\nThe Odd Numbers detected in the list are: ", odd_num_list)
-
+print ("\nThank you for your participation.")
